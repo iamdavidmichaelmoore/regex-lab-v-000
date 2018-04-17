08a774f7d955cd4c0fa34f0e097100d3fb5263f3
David Moore <hey@iamdavidmoore.com>
2018-04-17 15:32:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
+#method 1
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]/) != nil ? true : false
 end
 
+#method 2
 def words_starting_with_un_and_ending_with_ing(text)
-
+	text.scan(/[Uu]n\w+ing/)
 end
 
+#method 3
 def words_five_letters_long(text)
+  text.scan(/^[a-zA-Z]{0,5}$/)
 
 end
 
+#method 4
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match()
 end
 
+#method 5
 def valid_phone_number?(phone)
-
+	if phone.match(/[(]?(\d{3})[)]?[-_ ]?(\d{3})[-_ ]?(\d{4})/)
+		true
+	else
+		false
+	end
 end
